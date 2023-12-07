c7c96d4a7225cbc94433b9feffc0525d9b418b9b
Karanpartap Dhillon <kkarandhillonn@gmail.com>
2023-12-07 16:26:31 -0500

    v1

---
+++ 2.py
@@ -0,0 +1,19 @@
+x={}
+
+y=int(input("how many u want "))
+
+i=0
+while (y>0):
+    a=int(input("enter first"))
+    b=int(input("enter second"))
+    x.update({a.b})
+    y=y+1
+    
+    
+    
+print(x)    
+    
+
+
+ 
+
